eccc6a6c51a23b047da910a6cfd2540697926182
Jose Herrera <jaherreraf4@gmail.com>
2025-03-17 16:39:01 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterView } from 'vue-router'
+
+const links = [
+  { title: "Inicio", href: '#hero' },
+  { title: "Informacion", href: '#information' },
+  { title: "Iniciar Sección", href: '#login' },
+  { title: "Registrarse", href: '#register' },
+]
+</script>
+
+<template>
+  <div class="flex flex-col h-screen">
+    <header class="w-full p-4 flex items-center justify-between shadow-xs">
+      <img src="/public/parking-dark.svg" alt="" class="w-36 h-36">
+      <ul class="flex items-center justify-between gap-6">
+        <li v-for="(link, key) in links" :key="key">
+          <a :href="link.href">{{ link.title }}</a>
+        </li>
+      </ul>
+      <button class="text-center bg-blue-400 p-4 rounded-md cursor-pointer">Contáctanos!</button>
+    </header>
+    <main class="flex-grow">
+      <RouterView />
+    </main>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+  <main class="w-full h-screen flex items-center justify-center">
+    <div id="hero" class="grid grid-cols-2 items-center justify-center w-full max-w-7xl px-4">
+      <section class="flex flex-col items-center justify-center text-center">
+        <h1 class="text-4xl font-bold mb-8">
+          Ahorra tu tiempo en pagos y largas colas, haz tu pago desde una app
+        </h1>
+        <div class="flex items-center justify-center gap-4">
+          <button class="bg-black text-white px-6 py-3 rounded-md font-bold hover:bg-gray-800 transition-colors">
+            Iniciar Sesión
+          </button>
+          <button class="bg-black text-white px-6 py-3 rounded-md font-bold hover:bg-gray-800 transition-colors">
+            Registrarse
+          </button>
+        </div>
+      </section>
+      <div class="flex items-center justify-center">
+        <img src="/public/parking.png" alt="Parking" class="max-w-full h-auto">
+      </div>
+    </div>
+  </main>
+</template>
